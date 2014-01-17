079f8cf607097acef3428fe2a16d13a84b203962
matts1 <mattstark75@gmail.com>
2014-01-17 15:51:51 +1100

    Turned application into tornado application - no longer using google app engine

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import os
-import webapp2
+import sys
 import jinja2
+import tornado.web
+import tornado.ioloop
 
-class MainHandler(webapp2.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.response.write(JINJA_ENVIRONMENT.get_template('templates/base.html').render())
+        self.write(JINJA_ENVIRONMENT.get_template('viewcourses.html').render())
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-], debug=True)
+def path(x):
+    return (
+        '/' + x + '/(.*)',
+        tornado.web.StaticFileHandler,
+        {'path': os.path.join(os.path.dirname(__file__), 'static', x)}
+    )
+
+handlers = [
+    (r"/", MainHandler),
+    path('js'),
+    path('css'),
+    path('images'),
+    path('fonts'),
+    path(r'favicon\.ico'), # matches favicon.ico/(.*), but meh
+]
+
+if __name__ == '__main__':
+    settings = dict(
+        debug='debug' in sys.argv[1:],
+    )
+    app = tornado.web.Application(handlers, **settings)
+    app.listen(8080)
+    print '--------------------'
+    tornado.ioloop.IOLoop.instance().start()
